9febe5c66076186f558cca835a64c23851af8322
Carl Nordlund <carno10@liu.se>
2025-11-20 23:02:21 +0100

    Excluding /R/Lib

--- Threadle.CLIconsole/threadleR/R/threadle_client.R
+++ Threadle.CLIconsole/threadleR/R/threadle_client.R
@@ -12,7 +12,7 @@ library(jsonlite)
 #' @param path Path to the Threadle CLI executable.
 #' @return Invisibly returns the `processx` process object.
 #' @export
-.start_threadle <- function(path = "Threadle.CLIconsole.exe") {
+th_start_threadle <- function(path = "Threadle.CLIconsole.exe") {
   if (exists(".threadle_proc", envir=.GlobalEnv)) {
     stop("'.threadle_proc' process already running.")
   }
@@ -28,7 +28,7 @@ library(jsonlite)
 #'
 #' @return None; prints status messages.
 #' @export
-.stop_threadle <- function() {
+th_stop_threadle <- function() {
   if (exists(".threadle_proc", envir=.GlobalEnv)) {
     proc <- get(".threadle_proc", envir=.GlobalEnv)
     
@@ -78,6 +78,8 @@ library(jsonlite)
     out
 }
 
+
+
 #' Set working directory inside the Threadle CLI environment
 #'
 #' @param dir Path to the directory.
@@ -85,62 +87,64 @@ library(jsonlite)
 #' @examples
 #' set_workdir("~/data")
 #' @export
-set_workdir <- function(dir) {
+th_set_workdir <- function(dir) {
   .send_command(sprintf("setwd(dir=\"%s\")", dir))
 }
+
 
 #' Get the current working directory from Threadle CLI
 #'
 #' @return The working directory as returned by Threadle.
 #' @export
-get_workdir <- function() {
+th_get_workdir <- function() {
   .send_command(sprintf("getwd()"))
   #out <- .send_command(sprintf("getwd()"))
   #out[1]
 }
 
-#' View an object inside the Threadle CLI environment
+#' View a structure by variable in the Threadle CLI environment
 #'
 #' @param name Name of the object to view.
 #' @return CLI output.
 #' @export
-view <- function(name) {
-  .send_command(sprintf("view(%s)", name))
+th_view <- function(name) {
+  .send_command(sprintf("view(structure=%s)", name))
 }
 
-#' Create a new nodeset in Threadle
+#' Create a new nodeset in Threadle and assign it to variable 'name' in the Threadle CLI environment
 #'
 #' @param name Name of the R variable to assign in the CLI environment.
 #' @return A `threadle_nodeset` object.
 #' @export
-create_nodeset <- function(name) {
+th_create_nodeset <- function(name) {
   .send_command(sprintf("%s = createnodeset()", name))
   structure(list(name=name), class="threadle_nodeset")
 }
 
+#' Create a new network in Threadle and assign it to variable 'name' in the Threadle CLI environment
 #' Create a new network in Threadle
 #'
-#' @param name Name of the network variable.
+#' @param name Name of the assigned variable in the Threadle CLI environment.
 #' @param nodeset A `threadle_nodeset` object.
-#' @param label Optional display label.
+#' @param label Optional internal name of network in Threadle.
 #'
 #' @return A `threadle_network` object.
 #' @export
-create_network <- function(name, nodeset, label = NULL) {
+th_create_network <- function(name, nodeset, label = NULL) {
   label_arg <- if (!is.null(label)) sprintf(",name=%s", label) else ""
   .send_command(sprintf("%s = createnetwork(nodeset=%s%s)", name, nodeset$name, label_arg))
   structure(list(name = name), class = "threadle_network")
 }
 
-#' Load a file into Threadle
+#' Load a file into Threadle and assigns the structure(s) to the provided variable name in the Threadle CLI environment
 #'
-#' @param name Name for the resulting object.
+#' @param name Name of the assigned variable in the Threadle CLI environment.
 #' @param file File path.
-#' @param type Type of structure (e.g. "network", "nodeset").
+#' @param type Type of structure ("network" or "nodeset").
 #'
 #' @return An object with class corresponding to the loaded type.
 #' @export
-load_file <- function(name, file, type) {
+th_load_file <- function(name, file, type) {
   .send_command(sprintf("%s = loadfile(file=\"%s\", type=%s)",name, file, type))
   structure(list(name=name), class=paste0("threadle_",type))
 }
@@ -152,46 +156,45 @@ load_file <- function(name, file, type) {
 #'
 #' @return A `threadle_network` object.
 #' @export
-load_network <- function(name, file) {
+th_load_network <- function(name, file) {
   .send_command(sprintf("%s = loadfile(file=\"%s\", type=network)",name, file))
   structure(list(name=name, nodeset=paste0(name,"_nodeset"), class="threadle_network"))
   #structure(list(name=paste0(name,"_nodeset"), class="threadle_nodeset"))
 }
 
-#' Retrieve information from a Threadle object
+#' Retrieve meta information from a Threadle object
 #'
-#' @param name Name of the structure.
-#' @param format Output format ("json" or raw text).
+#' @param structure A `threadle_nodeset` or `threadle_network` object.
+#' @param format Output format ("json"(default) or "console").
 #'
-#' @return Parsed JSON (list) or raw CLI text.
+#' @return Parsed JSON or raw CLI text.
 #' @export
-info <- function(name, format="json") {
-  retval <- .send_command(sprintf("info(name=%s, format=%s)",name, format))
+th_info <- function(structure, format="json") {
+  retval <- .send_command(sprintf("info(structure=%s, format=%s)",structure$name, format))
   if (format =="json")
     fromJSON(retval)
   else
     retval
 }
 
-#' Add a node to a nodeset
+#' Add a node to a nodeset (or network)
 #'
-#' @param nodeset A `threadle_nodeset` object.
+#' @param structure A `threadle_nodeset` or `threadle_network` object.
 #' @param id Node ID.
 #'
 #' @return CLI output.
 #' @export
-add_node <- function(nodeset, id) {
-  .send_command(sprintf("addnode(nodeset=%s, id=%d)", nodeset$name, id))
-  #invisible(nodeset)
+th_add_node <- function(structure, id) {
+  .send_command(sprintf("addnode(structure=%s, id=%d)", structure$name, id))
 }
 
-#' List all objects currently defined in Threadle
+#' List all objects currently stored as variables in Threadle
 #'
-#' @param format Output format ("json" or raw text).
+#' @param format Output format ("json"(default) or console").
 #'
 #' @return Parsed JSON or raw text.
 #' @export
-inventory <- function(format = "json") {
+th_inventory <- function(format = "json") {
   retval <- .send_command(sprintf("i(format=json)"))
   if (format == "json")
     fromJSON(retval)
@@ -199,84 +202,81 @@ inventory <- function(format = "json") {
     retval
 }
 
-#' Define an attribute for a nodeset
+#' Define an attribute for a nodeset (or network)
 #'
-#' @param nodeset A `threadle_nodeset` object.
+#' @param structure A `threadle_nodeset` or `threadle_network` object.
 #' @param attrname Name of the attribute.
-#' @param attrtype Attribute type.
+#' @param attrtype Attribute type ("int","float", "char" or "bool")
 #'
 #' @return CLI output.
 #' @export
-define_attr <- function(nodeset, attrname, attrtype) {
-  cli <- sprintf("defineattr(nodeset=%s, attrname=%s, attrtype=%s)", nodeset$name, attrname, attrtype)
+th_define_attr <- function(structure, attrname, attrtype) {
+  cli <- sprintf("defineattr(structure=%s, attrname=%s, attrtype=%s)", structure$name, attrname, attrtype)
   .send_command(cli)
 }
 
-#' Set the value of a node attribute
+#' Set the value of a node attribute for a nodeset (or network)
 #'
-#' @param nodeset A `threadle_nodeset` object.
+#' @param structure A `threadle_nodeset` or `threadle_network` object.
 #' @param nodeid Node ID.
 #' @param attrname Attribute name.
 #' @param attrvalue Value to assign.
 #'
 #' @return CLI output.
 #' @export
-set_attr <- function(nodeset, nodeid, attrname, attrvalue) {
-  cli <- sprintf("setattr(nodeset=%s,nodeid=%d,attrname=%s,attrvalue=%s)",nodeset$name,nodeid,attrname, attrvalue)
+th_set_attr <- function(structure, nodeid, attrname, attrvalue) {
+  cli <- sprintf("setattr(structure=%s,nodeid=%d,attrname=%s,attrvalue=%s)",structure$name,nodeid,attrname, attrvalue)
   .send_command(cli)
 }
 
-#' Get the value of a node attribute
+#' Get the value of a node attribute for a nodeset (or network)
 #'
-#' @param nodeset Nodeset name or object.
+#' @param structure A `threadle_nodeset` or `threadle_network` object.
 #' @param nodeid Node ID.
 #' @param attrname Attribute name.
 #'
 #' @return CLI output.
 #' @export
-get_attr <- function(nodeset, nodeid, attrname) {
-  #getattr(nodeset=[var:nodeset],nodeid=[nodeid],attrname=[attributeName])
-  cli <- sprintf("getattr(structure=%s,nodeid=%d,attrname=%s)",nodeset,nodeid,attrname)
-  #print(cli)
+th_get_attr <- function(structure, nodeid, attrname) {
+  cli <- sprintf("getattr(structure=%s,nodeid=%d,attrname=%s)",structure$name,nodeid,attrname)
   .send_command(cli)
 }
 
-#' Define a layer in a network
+#' Adds/defines a relational layer in a network
 #'
 #' @param network A `threadle_network` object.
 #' @param layername Name of the layer.
 #' @param mode Layer mode (1 or 2).
-#' @param directed Logical; whether ties are directed.
-#' @param valuetype "binary" or "valued".
-#' @param selfties Logical; whether self-ties are allowed.
+#' @param directed Logical; whether ties are directed (only for 1-mode layers).
+#' @param valuetype "binary" or "valued" (only for 1-mode layers).
+#' @param selfties Logical; whether self-ties are allowed (only for 1-mode layers).
 #'
 #' @return CLI output.
 #' @export
-define_layer <- function(network, layername, mode, directed=FALSE, valuetype="binary", selfties=FALSE) {
-  # definelayer(network=[var:network], layername=[layerName], mode=['1','2'], *directed=['true','false'], *valuetype=['binary','valued'], *selfties=['true','false'])
-  cli <- sprintf("definelayer(network=%s, layername=%s, mode=%d, directed=%s, valuetype=%s, selfties=%s)", network$name, layername, mode, directed,valuetype,selfties)
+th_add_layer <- function(network, layername, mode, directed=FALSE, valuetype="binary", selfties=FALSE) {
+  cli <- sprintf("addlayer(network=%s, layername=%s, mode=%d, directed=%s, valuetype=%s, selfties=%s)", network$name, layername, mode, directed,valuetype,selfties)
   .send_command(cli)
 }
 
 #' Get the number of nodes in a structure
 #'
-#' @param name Name of the structure.
+#' @param name Name of the structure (can be a network or nodeset).
 #'
 #' @return A numeric value.
 #' @export
-get_nbr_nodes <- function(name) {
+th_get_nbr_nodes <- function(name) {
   cli <- sprintf("getnbrnodes(structure=%s)",name)
   as.numeric(.send_command(cli))
 }
 
 #' Get a node ID by index
 #'
-#' @param name Name of the structure.
+#' @param name Name of the structure (can be network or nodeset).
 #' @param index Numeric index.
 #'
 #' @return The node ID.
 #' @export
-get_nodeid_by_index <- function(name, index) {
+th_get_nodeid_by_index <- function(name, index) {
   cli <- sprintf("getnodeidbyindex(structure=%s, index=%d)",name, index)
   as.numeric(.send_command(cli))
 }
@@ -290,7 +290,7 @@ get_nodeid_by_index <- function(name, index) {
 #'
 #' @return Parsed JSON list of alters.
 #' @export
-get_node_alters <- function(name,layername,nodeid,direction="both") {
+th_get_node_alters <- function(name,layername,nodeid,direction="both") {
   cli <- sprintf("getnodealters(network=%s, layername=%s,nodeid=%d, direction=%s)",name, layername, nodeid, direction)
   fromJSON(.send_command(cli))
 }
@@ -299,13 +299,13 @@ get_node_alters <- function(name,layername,nodeid,direction="both") {
 #'
 #' @param name Network name.
 #' @param nodeid Node ID.
-#' @param layername Optional layer.
-#' @param direction Direction ("both", "in", "out").
-#' @param balanced Whether selection should be balanced.
+#' @param layername Optional layer. If left blank, will pick from all layers
+#' @param direction Direction ("both"(default), "in", "out").
+#' @param balanced Whether selection should be balanced for multiple layers.
 #'
 #' @return A node ID (numeric).
 #' @export
-get_random_alter <- function(name, nodeid, layername="", direction="both", balanced="false") {
+th_get_random_alter <- function(name, nodeid, layername="", direction="both", balanced="false") {
   cli <- sprintf("getrandomalter(network=%s, nodeid=%d, layername=%s, direction=%s, balanced=%s)",name, nodeid, layername,direction,balanced)
   as.numeric(.send_command(cli))
 }
@@ -315,7 +315,7 @@ get_random_alter <- function(name, nodeid, layername="", direction="both", balan
 #' @param name Structure name.
 #' @return A node ID (numeric).
 #' @export
-get_random_node <- function(name) {
+th_get_random_node <- function(name) {
   cli <- sprintf("getrandomnode(structure=%s)",name)
   as.numeric(.send_command(cli))
 }
