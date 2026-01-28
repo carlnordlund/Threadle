db9edbe224fd54ae706dbfa468e77f1a7883aa52
Carl Nordlund <carno10@liu.se>
2026-01-28 21:02:36 +0100

    Updated helptext for getallnodes()

--- Threadle.CLIconsole/Commands/GetAllNodes.cs
+++ Threadle.CLIconsole/Commands/GetAllNodes.cs
@@ -19,12 +19,12 @@ namespace Threadle.CLIconsole.Commands
         /// <summary>
         /// Gets the command syntax definition as shown in help and usage output.
         /// </summary>
-        public string Syntax => "getallnodes(structure = [var:structure], *offset = [int(default:0)], *limit = [int(default:1000])";
+        public string Syntax => "[array:str] = getallnodes(structure = [var:structure], *offset = [int(default:0)], *limit = [int(default:1000])";
 
         /// <summary>
         /// Gets a human-readable description of what the command does.
         /// </summary>
-        public string Description => "Returns a JSON-ready vector of the node ids of all nodes in the Nodeset (or the nodeset of the provided Network) that has the variable name [var:structure]. Will return maximum 1000 by default, but this can be changed with the 'limit' argument. Starts with the first node (index zero) by default, but this can be adjusted with 'offset'. Thus, if there are more than 1000 edges in the layer, all can be obtained either by using 'offset' for pagination and/or increasing the 'limit'. Do note that the node order reflects the order in which they were added, which thus may or may not be the same as their node ids.";
+        public string Description => "Returns a JSON-ready vector of the node ids of all nodes in the Nodeset (or the nodeset of the provided Network) that has the variable name [var:structure]. Will return maximum 1000 by default, but this can be changed with the 'limit' argument. Starts with the first node (index zero) by default, but this can be adjusted with 'offset'. Thus, if there are more than 1000 edges in the layer, all can be obtained either by using 'offset' for pagination and/or increasing the 'limit'. Do note that the node order reflects the order in which they were added, which thus may or may not be the same as their node ids. Also note that node attributes are not returned with this command.";
 
         /// <summary>
         /// Gets a value indicating whether this command produces output that must be assigned to a variable.
