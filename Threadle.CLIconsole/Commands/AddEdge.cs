8d188b9463d3a5db66d0d017ecf7ef3a2b6aae14
Carl Nordlund <carno10@liu.se>
2026-02-02 03:16:19 +0100

    Fixed correct 'node1id' and 'node2id' in command argument names.

--- Threadle.CLIconsole/Commands/AddEdge.cs
+++ Threadle.CLIconsole/Commands/AddEdge.cs
@@ -19,12 +19,12 @@ namespace Threadle.CLIconsole.Commands
         /// <summary>
         /// Gets the command syntax definition as shown in help and usage output.
         /// </summary>
-        public string Syntax => "addedge(network = [var:network], layername = [str], node1Id = [uint], node2Id = [uint], *value = [float(default:1)], *addmissingnodes = ['true'(default), 'false'])";
+        public string Syntax => "addedge(network = [var:network], layername = [str], node1id = [uint], node2id = [uint], *value = [float(default:1)], *addmissingnodes = ['true'(default), 'false'])";
 
         /// <summary>
         /// Gets a human-readable description of what the command does.
         /// </summary>
-        public string Description => "Creates and adds an edge between node1Id and node2Id in the specified layer 'layername' (which must be 1-mode) in network 'network'. If the layer is directional, node1Id is the source (from) and node2Id is the destination (to). The edge value 'value' is optional, defaulting to 1. If the specified node id's do not exist in the Nodeset, they are by default created and added, but by setting 'addmissingnodes' to 'false' prevents this.";
+        public string Description => "Creates and adds an edge between node1id and node2id in the specified layer 'layername' (which must be 1-mode) in network 'network'. If the layer is directional, node1id is the source (from) and node2id is the destination (to). The edge value 'value' is optional, defaulting to 1. If the specified node id's do not exist in the Nodeset, they are by default created and added, but by setting 'addmissingnodes' to 'false' prevents this.";
 
         /// <summary>
         /// Gets a value indicating whether this command produces output that must be assigned to a variable.
@@ -41,8 +41,8 @@ namespace Threadle.CLIconsole.Commands
             if (CommandHelpers.TryGetVariable<Network>(context, command.GetArgumentThrowExceptionIfMissingOrNull("network", "arg0"), out var network) is CommandResult commandResult)
                 return commandResult;
             string layerName = command.GetArgumentThrowExceptionIfMissingOrNull("layername", "arg1");
-            uint node1Id = command.GetArgumentParseUintThrowExceptionIfMissingOrNull("node1Id", "arg2");
-            uint node2Id = command.GetArgumentParseUintThrowExceptionIfMissingOrNull("node2Id", "arg3");
+            uint node1Id = command.GetArgumentParseUintThrowExceptionIfMissingOrNull("node1id", "arg2");
+            uint node2Id = command.GetArgumentParseUintThrowExceptionIfMissingOrNull("node2id", "arg3");
             float value = command.GetArgumentParseFloat("value", 1);
             bool addMissingNodes = command.GetArgumentParseBool("addmissingnodes", true);
             return CommandResult.FromOperationResult(network.AddEdge(layerName, node1Id, node2Id, value, addMissingNodes));
