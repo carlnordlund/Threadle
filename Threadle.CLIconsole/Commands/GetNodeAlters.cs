7234fad1262ba054df2a9f361796b976ee3e1ebc
Carl Nordlund <carno10@liu.se>
2026-02-16 00:38:51 +0100

    Modified getnodealters(): change default to unique=true (was false)

--- Threadle.CLIconsole/Commands/GetNodeAlters.cs
+++ Threadle.CLIconsole/Commands/GetNodeAlters.cs
@@ -15,12 +15,12 @@ namespace Threadle.CLIconsole.Commands
         /// <summary>
         /// Gets the command syntax definition as shown in help and usage output.
         /// </summary>
-        public string Syntax => "[array:uint] = getnodealters(network = [var:network], nodeid = [uint], *layernames = [semicolon-separated layer names], *direction = ['both'(default),'in','out'], *unique = [false(default),true])";
+        public string Syntax => "[array:uint] = getnodealters(network = [var:network], nodeid = [uint], *layernames = [semicolon-separated layer names], *direction = ['both'(default),'in','out'], *unique = [false,true(default)])";
 
         /// <summary>
         /// Gets a human-readable description of what the command does.
         /// </summary>
-        public string Description => "Get the id of the alters to a specific node in the Network with the variable name [var:network]. Will either return the node alters for one or more specified layers (as given by layernames) or return alters in all layers. When specifying multiple layers, separate their names with a semicolon (;). Output is in standard JSON array format. By default, both in- and outbound ties are included in the set of alters, but this can be adjusted with the optional direction argument. When obtaining alters from multiple (or all) layers, the same alter node might appear in several layers. By default, such alter nodes will then appear multiple times in the returned array. However, by setting 'unique' to true, this array will be deduplicated before returned. (As a necessary side-effect of deduplication, the list will also be sorted, which it is not otherwise by design). Note: there is nothing stopping you from naming the same layer multiple times in the layernames string.";
+        public string Description => "Get the id of the alters to a specific node in the Network with the variable name [var:network]. Will either return the node alters for one or more specified layers (as given by layernames) or return alters in all layers. When specifying multiple layers, separate their names with a semicolon (;). Output is in standard JSON array format. By default, both in- and outbound ties are included in the set of alters, but this can be adjusted with the optional direction argument. When obtaining alters from multiple (or all) layers, the same alter node might appear in several layers. By default, such alter nodes will not appear multiple times in the returned array. However, by setting 'unique' to false, an alter node id will appear the number of times that it appears in different layers. (As a necessary side-effect of deduplication, the list will also be sorted, which it is not otherwise by design). Note: there is nothing stopping you from naming the same layer multiple times in the layernames string.";
 
         /// <summary>
         /// Gets a value indicating whether this command produces output that must be assigned to a variable.
@@ -38,7 +38,7 @@ namespace Threadle.CLIconsole.Commands
                 return commandResult;
             uint nodeId = command.GetArgumentParseUintThrowExceptionIfMissingOrNull("nodeid", "arg1");
             string layerNames = command.GetArgumentParseString("layernames", "");
-            bool uniqueAlters = command.GetArgumentParseBool("unique", false);
+            bool uniqueAlters = command.GetArgumentParseBool("unique", true);
             string[]? layers = (layerNames.Length > 0) ? layerNames.Split(';') : null;
             EdgeTraversal edgeTraversal = command.GetArgumentParseEnum<EdgeTraversal>("direction", EdgeTraversal.Both);
             OperationResult<uint[]> result = network.GetNodeAlters(layers, nodeId, edgeTraversal, uniqueAlters);
