a021572d2ea42f387394912b13befc5ad59bae00
Carl Nordlund <carno10@liu.se>
2026-02-16 16:24:35 +0100

    Minor fix syntax text for Dichotomize and Symmetrize.

--- Threadle.CLIconsole/Commands/Dichotomize.cs
+++ Threadle.CLIconsole/Commands/Dichotomize.cs
@@ -15,12 +15,12 @@ namespace Threadle.CLIconsole.Commands
         /// <summary>
         /// Gets the command syntax definition as shown in help and usage output.
         /// </summary>
-        public string Syntax => "dichotomize(network = [var:network], layername = [str], *cond = ['eq','ne','gt','lt','ge'(default),'le','isnull'(invalid),'notnull'(invalid)], *threshold = [float(default:1)], *truevalue = [float(default:1),'keep'], *falsevalue = [float(default:0),'keep'], *newlayername = [str])";
+        public string Syntax => "dichotomize(network = [var:network], layername = [str], *cond = ['eq','ne','gt','lt','ge'(default),'le'], *threshold = [float(default:1)], *truevalue = [float(default:1),'keep'], *falsevalue = [float(default:0),'keep'], *newlayername = [str])";
 
         /// <summary>
         /// Gets a human-readable description of what the command does.
         /// </summary>
-        public string Description => "Takes a valued 1-mode layer (which it is only applicable for) and dichotomizes it, creating and storing this as a new layer. By default, all values equal to or greater to 1 will be converted to a binary tie and all other values represent a missing tie. Both the condition, the threshold value and the values resulting from a true vs. a false evaluation can be modified. If truevalue and falsevalue are kept at their default values, i.e. either containing 1 or 0, the resulting layer will be a binary network, otherwise the resulting layer will be valued. The directionality of the new layer will remain the same as the original layer. The new layer will be named as the provided layer, with the addition '-dichotomized', but the name of the new layer can also be specified with the 'newlayername' argument.";
+        public string Description => "Takes a valued 1-mode layer (which it is only applicable for) and dichotomizes it, creating and storing this as a new layer. By default, all values equal to or greater to 1 will be converted to a binary tie and all other values represent a missing tie. Both the condition, the threshold value and the values resulting from a true vs. a false evaluation can be modified. If truevalue and falsevalue are kept at their default values, i.e. either containing 1 or 0, the resulting layer will be a binary network, otherwise the resulting layer will be valued. The directionality of the new layer will remain the same as the original layer. The new layer will be named as the provided layer, with the addition '-dichotomized', but the name of the new layer can also be specified with the 'newlayername' argument. Note that the conditions 'isnull' and 'notnull' are not valid here.";
 
         /// <summary>
         /// Gets a value indicating whether this command produces output that must be assigned to a variable.
--- Threadle.CLIconsole/Commands/Symmetrize.cs
+++ Threadle.CLIconsole/Commands/Symmetrize.cs
@@ -15,7 +15,7 @@ namespace Threadle.CLIconsole.Commands
         /// <summary>
         /// Gets the command syntax definition as shown in help and usage output.
         /// </summary>
-        public string Syntax => "symmetrize(network = [var:network], layername = [str], method = ['max'(default),'min','minnonzero','average','sum','product'], * newlayername = [str])";
+        public string Syntax => "symmetrize(network = [var:network], layername = [str], *method = ['max'(default),'min','minnonzero','average','sum','product'], *newlayername = [str])";
 
         /// <summary>
         /// Gets a human-readable description of what the command does.
