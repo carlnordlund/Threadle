8f0c887f6f7b2d9be6ca88e15028d70e66a05f05
Carl Nordlund <carno10@liu.se>
2026-03-27 14:16:56 +0100

    Spelling mistake in gexf serializer

--- Threadle.Core/Utilities/FileSerializerGexf.cs
+++ Threadle.Core/Utilities/FileSerializerGexf.cs
@@ -27,7 +27,7 @@ namespace Threadle.Core.Utilities
             using var writer = XmlWriter.Create(filepath, new XmlWriterSettings { Indent = true, Encoding = Encoding.UTF8 });
 
             writer.WriteStartDocument();
-            writer.WriteStartElement("Gexf", "http://Gexf.net/1.3");
+            writer.WriteStartElement("gexf", "http://gexf.net/1.3");
             writer.WriteAttributeString("version", "1.3");
 
             writer.WriteStartElement("graph");
