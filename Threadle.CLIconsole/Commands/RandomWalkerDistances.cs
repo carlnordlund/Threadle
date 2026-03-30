22bdaadcd5c2c495a6b5e80f5c268d3037780911
Carl Nordlund <carno10@liu.se>
2026-03-30 17:05:02 +0200

    Continued working on rw-based distances - but need to implement string node attributes first!

--- Threadle.CLIconsole/Commands/RandomWalkerDistances.cs
+++ Threadle.CLIconsole/Commands/RandomWalkerDistances.cs
@@ -1,6 +1,8 @@
 ﻿using Threadle.CLIconsole.Parsing;
 using Threadle.CLIconsole.Results;
 using Threadle.CLIconsole.Runtime;
+using Threadle.Core.Model;
+using Threadle.Core.Utilities;
 
 namespace Threadle.CLIconsole.Commands
 {
@@ -12,7 +14,7 @@ namespace Threadle.CLIconsole.Commands
         /// <summary>
         /// Gets the command syntax definition as shown in help and usage output.
         /// </summary>
-        public string Syntax => "[var:network] = rwdistances(network = [var:network], nodeattr = [str], steps = [uint],*layernames = [semicolon-separated], *walks = [float(default=1.0)], *balanced = ['false'(default),'true'], *weighted = ['false'(default),'true'], *backtrack = ['false'(default),'true'], *stdev = ['false'(default),'true'])";
+        public string Syntax => "[var:network] = rwdistances(network = [var:network], attrname = [str], maxsteps = [uint], *layernames = [semicolon-separated], *walkfactor = [float(default=1.0)], *balanced = ['false'(default),'true'], *weighted = ['false'(default),'true'], *backtrack = ['false'(default),'true'], *savesteps = ['false'(default),'true'])";
 
         /// <summary>
         /// Gets a human-readable description of what the command does.
@@ -22,7 +24,7 @@ namespace Threadle.CLIconsole.Commands
         /// <summary>
         /// Gets a value indicating whether this command produces output that must be assigned to a variable.
         /// </summary>
-        public bool ToAssign => false;
+        public bool ToAssign => true;
 
         /// <summary>
         /// Executes the command.
@@ -31,7 +33,38 @@ namespace Threadle.CLIconsole.Commands
         /// <param name="context">The <see cref="CommandContext"/> providing shared console variable memory.</param>
         public CommandResult Execute(CommandPackage command, CommandContext context)
         {
-            return CommandResult.Fail("NotImplemented", "This command is not yet implemented");
+            var assigned = new Dictionary<string, string>();
+            string variableName = command.GetAssignmentVariableNameThrowExceptionIfNull();
+            if (CommandHelpers.TryGetVariable<Network>(context, command.GetArgumentThrowExceptionIfMissingOrNull("network", "arg0"), out var network) is CommandResult commandResult)
+                return commandResult;
+            string attrName = command.GetArgumentThrowExceptionIfMissingOrNull("attrname", "arg1");
+            int maxSteps = command.GetArgumentParseIntThrowExceptionIfMissingOrNull("maxsteps", "arg2");
+            string layerNames = command.GetArgumentParseString("layernames", "");
+            string[]? layers = (layerNames.Length > 0) ? layerNames.Split(';') : null;
+            float walkfactor = command.GetArgumentParseFloat("walkfactor", 1.0f);
+            bool balanced = command.GetArgumentParseBool("balanced", false);
+            bool weighted = command.GetArgumentParseBool("weighted", false);
+            bool backtrack = command.GetArgumentParseBool("backtrack", false);
+            bool savesteps = command.GetArgumentParseBool("savesteps", false);
+
+            var result = Core.Analysis.Distance.RandomWalkNodeAttributeDistances(network, attrName, maxSteps, layers, walkfactor, balanced, weighted, backtrack, savesteps);
+            if (!result.Success)
+                return CommandResult.Fail(result.Code, result.Message);
+            StructureResult structures = result.Value!;
+            context.SetVariable(variableName, structures.MainStructure);
+            assigned[variableName] = structures.MainStructure.GetType().Name;
+            if (structures.AdditionalStructures.Count > 0)
+                foreach (var kvp in structures.AdditionalStructures)
+                {
+                    string additionalAssignedVariable = variableName + "_" + kvp.Key;
+                    context.SetVariable(additionalAssignedVariable, kvp.Value);
+                    assigned[additionalAssignedVariable] = kvp.Value.GetType().Name;
+                }
+            return CommandResult.Ok(
+                $"Random-walker-derived distances on attribute '{attrName}' stored in '{structures.MainStructure.Name}'",
+                null,
+                assigned
+                );
         }
     }
 }
---
+++ Threadle.Core/Analysis/Distance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Threadle.Core.Model;
+using Threadle.Core.Model.Enums;
+using Threadle.Core.Utilities;
+
+namespace Threadle.Core.Analysis
+{
+    public static class Distance
+    {
+
+        public static OperationResult<StructureResult> RandomWalkNodeAttributeDistances(Network network, string attrName, int maxSteps, string[]? layers, float walkfactor = 1.0f, bool balanced = false, bool weighted = false, bool backtrack = false, bool savesteps = false)
+        {
+            Nodeset nodeset = network.Nodeset;
+            if (!nodeset.NodeAttributeDefinitionManager.TryGetAttributeIndex(attrName, out byte attrIndex))
+                return OperationResult<StructureResult>.Fail("AttributeUnknown", $"Attribute '{attrName}' not found in nodeset '{nodeset.Name}'.");
+
+            NodeAttributeType attrType = nodeset.NodeAttributeDefinitionManager.IndexToType[attrIndex];
+
+            // Continue later when I have implemented string node attributes!
+
+
+
+
+            return OperationResult<StructureResult>.Fail("NotYetImplemented", $"RandomWalkNodeAttributeDistances not yet implemented");
+
+        }
+    }
+}
