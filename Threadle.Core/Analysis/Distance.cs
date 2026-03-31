6dfa7d346621e3bb11c4db09a48e898300c79786
Carl Nordlund <carno10@liu.se>
2026-04-01 00:03:11 +0200

    Additional implementations rw distances

--- Threadle.Core/Analysis/Distance.cs
+++ Threadle.Core/Analysis/Distance.cs
@@ -52,17 +52,76 @@ namespace Threadle.Core.Analysis
             // Define a 'label' attribute
             var defineResult = nodesetResults.DefineNodeAttribute("label", NodeAttributeType.String);
             byte labelIndex = defineResult.Value;
-
+            Dictionary<string, uint> nodeAttributeStringToNodeId = [];
             for (uint i = 0; i < (uint)labels.Length; i++)
             {
                 int poolIndex = nodesetResults.GetOrAddStringToPool(labels[i]);
                 nodesetResults._addNodeWithAttributes(i, (new List<byte> { labelIndex }, new List<NodeAttributeValue> { new NodeAttributeValue(poolIndex) }));
+                nodeAttributeStringToNodeId[labels[i]] = i;
             }
-            // Continue later when I have implemented string node attributes!
-
+            // Create network for results
             Network networkResults = new Network(attrName + "_rwdistances_results", nodesetResults);
 
+            // Determine how many nodes should be included in each step level
+            // This is related to walkFactor: if walkFactor=0.5, only every second should be included
+            // if walkfactor = 3, each node should be included 3 times
+            int nbrNodesPerStepLevel = (int)(nodeset.Count * walkfactor);
 
+            // Iterate through all with given path lengths (i.e. s as number of steps
+            for (int s = 1; s <= maxSteps; s++)
+            {
+                Dictionary<(uint, uint), int> resultsDict = [];
+                for (int i = 0; i < nbrNodesPerStepLevel; i++)
+                {
+                    uint nodeIndex = (uint)Math.Floor(i / walkfactor);
+                    if (!(nodeset.GetNodeIdByIndex(nodeIndex) is uint egoNodeId))
+                        continue;
+
+                    // Get the attr value for ego
+                    string startNodeAttrString = nodeset.GetNodeAttribute(egoNodeId, attrIndex) is NodeAttributeValue navStart ? navStart.ToString(attrType) : "(missing)";
+
+                    // Now take s steps 
+                    bool abort = false;
+                    uint currentNodeId = egoNodeId;
+                    for (int j = 0; j < s; j++)
+                    {
+                        // Get an alter node from the current node
+                        var randomAlterResult = Analyses.GetRandomAlter(network, currentNodeId, layers![0], EdgeTraversal.Out, balanced, weighted);
+                        // If this didn't work, e.g. no more alters in that direction, then abort the whole walk
+                        if (!randomAlterResult.Success)
+                        {
+                            abort = false;
+                            break;
+                        }
+                        // As long as the alter node is not back at the start, update the current one
+                        // An alternative would be to abort this walk completely
+                        if (randomAlterResult.Value != egoNodeId)
+                            currentNodeId = randomAlterResult.Value;
+
+                    }
+
+                    // If this was aborted: move to next node, just skip this one
+                    if (abort)
+                        continue;
+                        
+                    // Ok, done s walks: now I should store s
+                    string endNodeAttrString = nodeset.GetNodeAttribute(currentNodeId, attrIndex) is NodeAttributeValue navEnd ? navEnd.ToString(attrType) : "(missing)";
+
+                    uint nodeIdFrom = nodeAttributeStringToNodeId[startNodeAttrString];
+                    uint nodeIdTo = nodeAttributeStringToNodeId[endNodeAttrString];
+                    if (resultsDict.TryGetValue((nodeIdFrom, nodeIdTo), out int value))
+                        resultsDict[(nodeIdFrom, nodeIdTo)] = value + 1;
+                    else
+                        resultsDict[(nodeIdFrom, nodeIdTo)] = 1;
+                }
+
+                LayerOneMode resultLayer = new LayerOneMode(attrName + "_steps_" + s, EdgeDirectionality.Directed, EdgeType.Valued, true);
+                foreach (var kvp in resultsDict)
+                    resultLayer.AddEdge(kvp.Key.Item1, kvp.Key.Item2, kvp.Value);
+                networkResults.Layers.Add(resultLayer.Name, resultLayer);
+            }
+
+            // Return results
             StructureResult results = new StructureResult(networkResults, new Dictionary<string, IStructure> { { "nodeset", nodesetResults } });
 
             return OperationResult<StructureResult>.Ok(results, "Random walker done!");
